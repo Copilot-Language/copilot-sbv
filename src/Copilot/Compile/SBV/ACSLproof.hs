d2d1005e39ab5280534469ed08995dcd4745d896
lauratitolo <laura.titolo@nianet.org>
2016-03-22 10:00:34 -0400

    wip

--- src/Copilot/Compile/SBV/ACSLproof.hs
+++ src/Copilot/Compile/SBV/ACSLproof.hs
@@ -6,7 +6,7 @@
 
 module Copilot.Compile.SBV.ACSLproof
   ( transformProofACSL
-  )
+  ) 
 where
 
 import Prelude hiding (id)
@@ -86,7 +86,7 @@ transformTrigger Trigger
 --data Property = Property
 --  { propertyName     :: Name
 --  , propertyExpr     :: Expr Bool }
-
+  
 
 transformProperty :: Property -> Property
 transformProperty Property
@@ -128,7 +128,7 @@ transformProofACSL Spec
 --  { uExprType :: Type a
 --  , uExprExpr :: Expr a }
 transformUExpr :: UExpr -> UExpr
-transformUExpr UExpr { uExprExpr = e, uExprType = t } =
+transformUExpr UExpr { uExprExpr = e, uExprType = t } = 
   UExpr { uExprExpr = transformExpr e, uExprType = t }
 
 
@@ -143,36 +143,34 @@ transformUExpr UExpr { uExprExpr = e, uExprType = t } =
 --  Const        :: Type a -> a -> Expr a
 --  Drop         :: Type a -> DropIdx -> Id -> Expr a
 --  Local        :: Type a -> Type b -> Name -> Expr a -> Expr b -> Expr b
---  Var          :: Type a -> Name -> Expr a
---  ExternVar    :: Type a -> Name -> Maybe [a] -> Expr a
---  ExternFun    :: Type a -> Name -> [UExpr] -> Maybe (Expr a)
+--  Var          :: Type a -> Name -> Expr a 
+--  ExternVar    :: Type a -> Name -> Maybe [a] -> Expr a 
+--  ExternFun    :: Type a -> Name -> [UExpr] -> Maybe (Expr a) 
 --               -> Maybe Tag -> Expr a
 --  ExternArray  :: Integral a => Type a -> Type b -> Name -> Int -> Expr a
---               -> Maybe [[b]] -> Maybe Tag -> Expr b
---  Op1          :: Op1 a b -> Expr a -> Expr b
+--               -> Maybe [[b]] -> Maybe Tag -> Expr b 
+--  Op1          :: Op1 a b -> Expr a -> Expr b 
 --  Op2          :: Op2 a b c -> Expr a -> Expr b -> Expr c
 --  Op3          :: Op3 a b c d -> Expr a -> Expr b -> Expr c -> Expr d
 
-transformExpr :: Expr a -> Expr a
+transformExpr :: Expr a -> Expr a 
 transformExpr e0 = case e0 of
-  Const t x                       -> Const t x
-  Drop t k id                     -> Drop t k id
-  Local t1 t2 name e1 e2          -> Local t1 t2 name (transformExpr e1) (transformExpr e2)
-  Var t name                      -> Var t name
-  ExternVar t name e              -> ExternVar t name e
-  ExternFun t name args contxt yy -> ExternFun t name (map transformUExpr args) contxt yy
-  ExternArray t1 t2 name size idx context yy
-                                  -> ExternArray t1 t2 name size (transformExpr idx) context yy
-  ExternMatrix t1 t2 name rows cols idxr idxc context yy
-                                  -> ExternMatrix t1 t2 name rows cols (transformExpr idxr) (transformExpr idxc) context yy
-  Op1 op e                        -> transformOp1 op e
-  Op2 op e1 e2                    -> transformOp2 op e1 e2
-  Op3 op e1 e2 e3                 -> transformOp3 op e1 e2 e3
+  Const t x                      -> Const t x
+  Drop t k id                    -> Drop t k id
+  Local t1 t2 name e1 e2         -> Local t1 t2 name (transformExpr e1) (transformExpr e2) 
+  Var t name                     -> Var t name
+  ExternVar t name e             -> ExternVar t name e
+  ExternFun t name args contxt yy-> ExternFun t name (map transformUExpr args) contxt yy
+  ExternArray t1 t2 name 
+              size idx context yy-> ExternArray t1 t2 name size (transformExpr idx) context yy
+  Op1 op e                       -> transformOp1 op e
+  Op2 op e1 e2                   -> transformOp2 op e1 e2
+  Op3 op e1 e2 e3                -> transformOp3 op e1 e2 e3
 
-  Label t s e                    -> case s of
+  Label t s e                    -> case s of 
     '?':m -> ExternFun t ("ident_"++(showType t)) [UExpr {uExprExpr = transformExpr $ Label t m $ e, uExprType = t}] Nothing Nothing
     _     -> Label t s $ transformExpr e
-
+    
 
 showType :: Type a -> String
 showType t = case t of
@@ -196,67 +194,67 @@ transformOp1 op e = case op of
   Abs   t      -> Op2 (Mul t) (transformExpr e) (transformExpr $ Label t "?absolute_value_splitting" $ Op1 (Sign t) $ e)
   Sign  t      -> Op1 (Sign t) $ transformExpr e
   -- Fractional operators.
-  Recip t      -> Op2 (Fdiv t) (Const t 1.0) (transformExpr e)
+  Recip a      -> Op2 (Fdiv a) (Const a 1.0) (transformExpr e)
   -- Floating operators.
-  Exp Float    -> ExternFun Float "expf"
+  Exp Float    -> ExternFun Float "expf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Exp Double   -> ExternFun Double "exp"
+  Exp Double   -> ExternFun Double "exp" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Sqrt Float   -> ExternFun Float "sqrtf"
+  Sqrt Float   -> ExternFun Float "sqrtf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Sqrt Double  -> ExternFun Double "sqrt"
+  Sqrt Double  -> ExternFun Double "sqrt" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Log Float    -> ExternFun Float "logf"
+  Log Float    -> ExternFun Float "logf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Log Double   -> ExternFun Double "log"
+  Log Double   -> ExternFun Double "log" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Sin Float    -> ExternFun Float "sinf"
+  Sin Float    -> ExternFun Float "sinf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Sin Double   -> ExternFun Double "sin"
+  Sin Double   -> ExternFun Double "sin" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Cos Float    -> ExternFun Float "cosf"
+  Cos Float    -> ExternFun Float "cosf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Cos Double   -> ExternFun Double "cos"
+  Cos Double   -> ExternFun Double "cos" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Tan Float    -> ExternFun Float "tanf"
+  Tan Float    -> ExternFun Float "tanf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Tan Double   -> ExternFun Double "tan"
+  Tan Double   -> ExternFun Double "tan" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Asin Float   -> ExternFun Float "asinf"
+  Asin Float   -> ExternFun Float "asinf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Asin Double  -> ExternFun Double "asin"
+  Asin Double  -> ExternFun Double "asin" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Acos Float   -> ExternFun Float "acosf"
+  Acos Float   -> ExternFun Float "acosf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Acos Double  -> ExternFun Double "acos"
+  Acos Double  -> ExternFun Double "acos" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Atan Float   -> ExternFun Float "atanf"
+  Atan Float   -> ExternFun Float "atanf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Atan Double  -> ExternFun Double "atan"
+  Atan Double  -> ExternFun Double "atan" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Sinh Float   -> ExternFun Float "sinhf"
+  Sinh Float   -> ExternFun Float "sinhf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Sinh Double  -> ExternFun Double "sinh"
+  Sinh Double  -> ExternFun Double "sinh" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Cosh Float   -> ExternFun Float "coshf"
+  Cosh Float   -> ExternFun Float "coshf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Cosh Double  -> ExternFun Double "cosh"
+  Cosh Double  -> ExternFun Double "cosh" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Tanh Float   -> ExternFun Float "tanhf"
+  Tanh Float   -> ExternFun Float "tanhf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Tanh Double  -> ExternFun Double "tanh"
+  Tanh Double  -> ExternFun Double "tanh" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Asinh Float  -> ExternFun Float "asinhf"
+  Asinh Float  -> ExternFun Float "asinhf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Asinh Double -> ExternFun Double "asinh"
+  Asinh Double -> ExternFun Double "asinh" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Acosh Float  -> ExternFun Float "acoshf"
+  Acosh Float  -> ExternFun Float "acoshf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Acosh Double -> ExternFun Double "acosh"
+  Acosh Double -> ExternFun Double "acosh" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
-  Atanh Float  -> ExternFun Float "atanhf"
+  Atanh Float  -> ExternFun Float "atanhf" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Float }] Nothing Nothing
-  Atanh Double -> ExternFun Double "atanh"
+  Atanh Double -> ExternFun Double "atanh" 
                     [UExpr { uExprExpr = transformExpr e, uExprType = Double }] Nothing Nothing
   -- Bitwise operators.
   BwNot    t   -> Op1 (BwNot t) $ transformExpr e
@@ -278,11 +276,11 @@ transformOp2 op e1 e2 = case op of
   -- Fractional operators.
   Fdiv   t     -> Op2 (Fdiv t) (transformExpr e1) (transformExpr e2)
   -- Floating operators.
-  Pow    Float -> ExternFun Float "powf"
-                    [UExpr { uExprExpr = transformExpr e1, uExprType = Float }
+  Pow    Float -> ExternFun Float "powf" 
+                    [UExpr { uExprExpr = transformExpr e1, uExprType = Float } 
                     , UExpr { uExprExpr = transformExpr e2, uExprType = Float }] Nothing Nothing
-  Pow    Double-> ExternFun Double "pow"
-                    [UExpr { uExprExpr = transformExpr e1, uExprType = Double }
+  Pow    Double-> ExternFun Double "pow" 
+                    [UExpr { uExprExpr = transformExpr e1, uExprType = Double } 
                     , UExpr { uExprExpr = transformExpr e2, uExprType = Double }] Nothing Nothing
   Logb   t     -> Op2 (Fdiv t) (transformExpr $ Op1 (Log t) e1) (transformExpr $ Op1 (Log t) e2)
   -- Equality operators.
@@ -302,6 +300,6 @@ transformOp2 op e1 e2 = case op of
 
 
 transformOp3 :: Op3 a b c d -> Expr a -> Expr b -> Expr c -> Expr d
-transformOp3 op e1 e2 e3 = case op of
+transformOp3 op e1 e2 e3 = case op of 
   Mux   Bool   -> Op2 Or (transformExpr $ Op2 And (e2) (e1)) (transformExpr $ Op2 And (e3) (Op1 Not e1))
-  Mux   t      -> Op3 (Mux t) (transformExpr e1) (transformExpr e2) (transformExpr e3)
+  Mux   t      -> Op3 (Mux t) (transformExpr e1) (transformExpr e2) (transformExpr e3) 
